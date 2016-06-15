6a4c80e1717558a09112de2d0efd135d68a45f17
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2016-06-15 17:21:48 +0200

    helper function
    Just in case, I doubt about its utility

--- js/src/helpers/functions.js
+++ js/src/helpers/functions.js
@@ -1,19 +1,34 @@
 
+var _ = require('lodash');
+
 var FunctionHelper = {
 
-  debounce: function (func, wait, immediate) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
+  concatenateCountries: function(array) {
+    var string = '',
+      compactedArray = _.compact(array),
+      arrayLength = compactedArray.length;
+
+    if (arrayLength == 0) {
+      return;
+    }
+
+    if (arrayLength == 1) {
+      if(array[0]) {
+        string = " iso='" + array[0] + "'";
+      }
+    }
+
+    compactedArray.forEach(function(elem, index) {
+
+      if (arrayLength - 1 == index) {
+        string += "iso='" + elem + "'";
+      } else {
+        string += " iso='" + elem + "' or ";
+      }
+
+    });
+
+    return string;
   },
 
   scrollTop: function() {
