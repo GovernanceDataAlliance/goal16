9c17ebb096fd93b190bc8c14b5eca485b30266b1
Travis CI <claralinos@Claras-MacBook-Pro.local>
2016-06-20 18:45:10 +0200

    base url

--- js/src/main.js
+++ js/src/main.js
@@ -5,4 +5,8 @@ var helpers = require('./helpers/handlebars.js');
 var Router = require('./router.js'),
     router = new Router();
 
-Backbone.history.start({pushState: true});
+Backbone.history.start({
+  pushState: true,
+  root: SITEURL ? "/" + SITEURL : "/"
+});
+
