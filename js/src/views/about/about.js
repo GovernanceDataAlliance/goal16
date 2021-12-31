d7d81928a9dad78f0924d75fa87ce84a2fdb6fff
pfkl-chia254 <alexander@poetfarmer.com>
2021-12-31 10:42:43 +0800

    [fea] about page done

--- js/src/views/about/about.js
+++ js/src/views/about/about.js
@@ -1,6 +1,8 @@
 var Backbone = require('backbone'),
     Handlebars = require('handlebars');
 
+var AboutSelector = require('./about_selector.js');
+
 var AboutView = Backbone.View.extend({
 
   el: '#l-about',
@@ -9,7 +11,10 @@ var AboutView = Backbone.View.extend({
     'click .js-about-link' : 'onClickNavigate'
   },
 
-  initialize: function() {},
+  initialize: function() {
+    this._selectOptions();
+    $('.js-about-link').first().addClass('active');
+  },
 
   render: function() {
     return this;
@@ -21,12 +26,22 @@ var AboutView = Backbone.View.extend({
   onClickNavigate: function(e) {
     var $section = $(e.currentTarget).data('section');
     var $target = $('#'+ $section);
-    var margin = $section === "partners" ? 0 : 40;
+    var margin = $section === "partners" ? 40 : 90;
 
     $('html, body').animate({
         scrollTop: $target.offset().top - margin
     }, 1000);
-  }
+
+    $('.js-about-link').removeClass('active');
+    $(e.currentTarget).addClass('active');
+
+  },
+
+  _selectOptions: function() {
+    new AboutSelector({
+      el: '#aboutSelector'
+    });
+  },
 });
 
 module.exports = AboutView;
